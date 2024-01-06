3115bd5601ea5215ada5ce21e704d9bc427453bf
Akshay <akshay.kamble@copods.co>
2024-01-06 19:04:19 +0530

    ok

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,7 @@ const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
+// ok
 
 exports.helloWorld = onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
